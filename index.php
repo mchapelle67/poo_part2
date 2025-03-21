acab13bd4d928ffbdfc7bbcd71f8ea8515436201
Manon <manon.chp68@gmail.com>
2025-03-21 11:36:17 +0100

    exos

--- index.php
+++ index.php
@@ -15,23 +15,3 @@ $livre3 = new Livre("Le Fleau", "1978", "823", 14, $stephenKing);
 $livre4 = new Livre("Shinning", "1977", "447", 16, $stephenKing);
 
 echo $stephenKing->afficherBibliographie();
-
-
-
-
-
-// echo $livre1->afficherBibliographie();
-// echo $livre2->afficherBibliographie();
-// echo $livre3->afficherBibliographie();
-// echo $livre4->afficherBibliographie();
-
-
-
-// classe : moule pour creer un objet 
-
-// objet : c'est une instance d'une classe
-// encapsulation : restreint l'accès au objet privé ou protegé de la classe et pes protège d'un accès externe 
-
-// methode magique : ce sont des fonctions spécifique à la poo
-
-// $this : c'est la valeur de l'objet appelant 
